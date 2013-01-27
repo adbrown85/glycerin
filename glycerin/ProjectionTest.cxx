ba4e23c8016e16f7b008f0abcfede921d3361349
Andy Brown <adb1413@rit.edu>
2013-01-27 15:25:59 -0500

    Moved ProjectionTest out of Glycerin namespace (ref #23)

--- glycerin/ProjectionTest.cxx
+++ glycerin/ProjectionTest.cxx
@@ -22,7 +22,6 @@
 #include <m3d/Math.h>
 #include "glycerin/Projection.hxx"
 using namespace std;
-namespace Glycerin {
 
 
 /**
@@ -50,7 +49,7 @@ public:
         const double far = +1.0;
 
         // Compute orthographic projection matrix
-        M3d::Mat4 mat = Projection::orthographic(width, height);
+        M3d::Mat4 mat = Glycerin::Projection::orthographic(width, height);
 
         // Check first column
         CPPUNIT_ASSERT_DOUBLES_EQUAL(2.0 / (right - left), mat[0][0], TOLERANCE);
@@ -90,7 +89,7 @@ public:
         const double far = 100.0;
 
         // Compute perspective projection matrix
-        M3d::Mat4 mat = Projection::perspective(fov, aspect, near, far);
+        M3d::Mat4 mat = Glycerin::Projection::perspective(fov, aspect, near, far);
 
         // Compute f, which is the cotangent of field of view divived by 2
         const double f = 1.0 / tan(M3d::toRadians(fov / 2.0));
@@ -126,11 +125,9 @@ public:
     CPPUNIT_TEST_SUITE_END();
 };
 
-} /* namespace Glycerin */
-
 int main(int argc, char* argv[]) {
     CppUnit::TextUi::TestRunner runner;
-    runner.addTest(Glycerin::ProjectionTest::suite());
+    runner.addTest(ProjectionTest::suite());
     runner.run();
     return 0;
 }
