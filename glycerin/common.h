110f4bdef0f4f67b95c67447ab03ad3964839e3a
Andrew Brown <adb1413@rit.edu>
2013-04-23 01:14:02 -0400

    Don't redeclare GL3_PROTOTYPES and GL_GLEXT_PROTOTYPES (ref #34)

--- glycerin/common.h
+++ glycerin/common.h
@@ -19,10 +19,14 @@
 #define GLYCERIN_COMMON_H
 #include <cstdlib>
 #ifdef __APPLE__
+#ifndef GL3_PROTOTYPES
 #define GL3_PROTOTYPES
+#endif
 #include <OpenGL/gl3.h>
 #else
+#ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES
+#endif
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include <GL/glx.h>
